84f006144327053041967f426f3d68ccd66ff1bb
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2018-01-09 10:03:57 -0600

    compatibilidad con mcart 0.7

--- FormRender/Properties/AssemblyInfo.cs
+++ FormRender/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using MCART.Attributes;
 [assembly: AssemblyTrademark("Grupo Sinergia")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: MinMCARTVersion(0, 6, 1, 0)]
+[assembly: MinMCARTVersion(0, 7, 0, 0)]
 [assembly: TargetMCARTVersion(0, 7, 0, 0)]
